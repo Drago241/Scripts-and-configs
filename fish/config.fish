2d2d7ddbc3ae828b06b6ecf5588907f7bbc9ac3f
Muhammad Abdullah <78714956+Drago241@users.noreply.github.com>
2026-01-08 13:09:39 +0500

    Delete fish directory

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-# Add local binary path to the PATH variable
-set -gx PATH /home/$USER/.local/bin $PATH
-
-# Initialize Oh My Posh with your theme configuration
-oh-my-posh init fish --config /home/$USER/.cache/oh-my-posh/themes/kushal.omp.json | source
-
-set -U fish_greeting ""
-
-set -Ux fish_features no-keyboard-protocols
